140a466408e735df31237473baaca9d16ec7e3d1
Patcharapat Riankham <patcharapat.rk@gmail.com>
2020-12-24 23:35:17 +0700

    Add form Register & Login

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/register', function(req, res, next) {
-  res.render('Register');
+router.get('/register', function (req, res, next) {
+  res.render('register');
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('Login');
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+router.post('/register', [
+  check('email', 'กรุณาป้อนอีเมล').isEmail(),
+  check('name', 'กรุณาป้อนชื่อ').not().isEmpty(),
+  check('password', 'กรุณาป้อนรหัาผ่าน').not().isEmpty(),
+], function (req, res, next) {
+  const result = validationResult(req);
+  var errors = result.errors;
+  // Validation Data
+  if (!result.isEmpty()) {
+    // Return error to views
+    res.render('register', { errors: errors })
+  } else {
+    // Insert Data
+    res.render('login');
+  }
 });
 
 module.exports = router;
